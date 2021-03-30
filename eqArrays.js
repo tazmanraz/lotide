e6f3af692800fb2f208a6d4f6d5eefbc3a794e18
Imtiaz Miah <imtiazmiah@gmail.com>
2021-03-30 21:15:33 +0000

    added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return true;
+};
+
+const eqArrays = function(arr1, arr2){
+  let flag = true;
+  for (let i = 0; i < arr1.length; i++){
+    if (arr1[i] !== arr2[i]){
+      flag = false;
+    }
+  }
+  return flag;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
