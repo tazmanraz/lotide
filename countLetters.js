aa14e0bc9d12099a59af56d598c5f6b287a5ca8c
Imtiaz Miah <imtiazmiah@gmail.com>
2021-04-01 00:00:15 +0000

    add countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(word) {
+  const wordCount = {}
+
+  for (let i = 0;  i < word.length; i++){
+    let occurances = 0;
+  
+    for(let j = 0; j < word.length; ++j){
+      if(word[j] === word[i]){
+          occurances++;
+        }
+    }
+    wordCount[word[i]] = occurances;
+  }
+  console.log(wordCount)
+  return wordCount
+}
+
+countLetters('deeerp')
