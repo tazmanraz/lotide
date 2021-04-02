54053148ca2c10ee2047b82bffea002802d7047c
Imtiaz Miah <imtiazmiah@gmail.com>
2021-04-02 01:50:32 +0000

    initial commit of findKey.js

---
+++ findKey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return true;
+};
+
+const findKey = (object, callback) => {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+const test1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(test1, "noma");
