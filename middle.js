cce16d07e8da670ede5fdcb29b6f03f1ea4ae435
Imtiaz Miah <imtiazmiah@gmail.com>
2021-04-05 18:55:14 +0000

    refactored and added test

--- middle.js
+++ middle.js
@@ -1,27 +1,4 @@
-const eqArrays = function(arr1, arr2){
-  let flag = true;
-
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++){
-    if (arr1[i] !== arr2[i]){
-      flag = false;
-    }
-  }
-  return flag;
-}
-
-const assertArraysEqual = function(actual, expected) {
-
-  if (eqArrays(actual,expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// ACTUAL FUNCTION
+// This function returns the middle element(s) of an array.
 const middle = function(array) {
   let newArr=[]
   if (array.length === 1 || array.length === 2){
@@ -41,12 +18,4 @@ const middle = function(array) {
   return newArr
 }
 
-assertArraysEqual(
-  middle([1, 2, 3, 4, 5]),
-  [3]
-  );
-
-assertArraysEqual(
-  middle([1, 2, 3, 4, 5, 6]),
-  [3, 4]
-  );
+module.exports = middle;
