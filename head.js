bb9388f932db4af8506ae66ede6b7ea3e46414fa
Imtiaz Miah <imtiazmiah@gmail.com>
2021-04-05 18:12:12 +0000

    adding tests

--- head.js
+++ head.js
@@ -2,13 +2,5 @@ const head = function(arr) {
   return arr[0];
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+module.exports = head;
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
